3bb1699690f9734ab3fdf83a68459257b1ca8e32
Cristian Samuel Hernandez <craazycat213@gmail.com>
2026-08-11 23:58:54 -0400

    Added an arrow shape.

--- MathGrapher/src/general_shapes.rs
+++ MathGrapher/src/general_shapes.rs
@@ -78,3 +78,90 @@ pub fn draw_cylinder_rot(
     let matrix = translation * rotation_x * rotation_y * rotation_z;
     draw_cylinder_matrix(matrix, radius_top, radius_bottom, height, texture, color);
 }
+
+pub fn draw_cone_matrix(
+    matrix: Mat4,
+    radius: f32,
+    height: f32,
+    texture: Option<&Texture2D>,
+    color: Color
+) {
+    draw_cylinder_matrix(matrix, 0.0, radius, height, texture, color);
+}
+
+pub fn draw_cone_quat(
+    position: Vec3,
+    quaternion: Quat,
+    radius: f32,
+    height: f32,
+    texture: Option<&Texture2D>,
+    color: Color
+) {
+    draw_cylinder_quat(position, quaternion, 0.0, radius, height, texture, color);
+}
+
+pub fn draw_cone_rot(
+    position: Vec3,
+    rotation: Vec3,
+    radius: f32,
+    height: f32,
+    texture: Option<&Texture2D>,
+    color: Color
+) {
+    draw_cylinder_rot(position, rotation, 0.0, radius, height, texture, color);
+}
+
+pub fn draw_arrow_matrix(
+    matrix: Mat4,
+    radius_shaft: f32,
+    radius_tip: f32,
+    length: f32,
+    ratio: f32,
+    texture: Option<&Texture2D>,
+    color: Color
+) {
+    let shaft_height = ratio * length;
+    draw_cylinder_matrix(matrix, radius_shaft, radius_shaft, shaft_height, texture, color);
+
+    let translation_matrix = mat4(
+        vec4(1.0, 0.0, 0.0, 0.0),
+        vec4(0.0, 1.0, 0.0, 0.0),
+        vec4(0.0, 0.0, 1.0, 0.0),
+        vec4(0.0, shaft_height, 0.0, 1.0)
+    );
+    let new_matrix = matrix * translation_matrix;
+    let tip_height = (1.0 - ratio) * length;
+    draw_cone_matrix(new_matrix, radius_tip, tip_height, texture, color);
+}
+
+pub fn draw_arrow_quad(
+    position: Vec3,
+    quaternion: Quat,
+    radius_shaft: f32,
+    radius_tip: f32,
+    length: f32,
+    ratio: f32,
+    texture: Option<&Texture2D>,
+    color: Color
+) {
+    let matrix = Mat4::from_rotation_translation(quaternion, position);
+    draw_arrow_matrix(matrix, radius_shaft, radius_tip, length, ratio, texture, color);
+}
+
+pub fn draw_arrow_rot(
+    position: Vec3,
+    rotation: Vec3,
+    radius_shaft: f32,
+    radius_tip: f32,
+    length: f32,
+    ratio: f32,
+    texture: Option<&Texture2D>,
+    color: Color
+) {
+    let translation = Mat4::from_translation(position);
+    let rotation_x = Mat4::from_rotation_x(rotation.x);
+    let rotation_y = Mat4::from_rotation_y(rotation.y);
+    let rotation_z = Mat4::from_rotation_z(rotation.z);
+    let matrix = translation * rotation_x * rotation_y * rotation_z;
+    draw_arrow_matrix(matrix, radius_shaft, radius_tip, length, ratio, texture, color);
+}
