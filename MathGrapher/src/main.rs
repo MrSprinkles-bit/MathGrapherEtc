881d2008c53b207bf95b66f606c67bb9037e3703
Cristian Samuel Hernandez <craazycat213@gmail.com>
2026-08-12 00:01:48 -0400

    Major axis lines now use the new arrow shape.

--- MathGrapher/src/main.rs
+++ MathGrapher/src/main.rs
@@ -17,12 +17,6 @@ async fn main() {
     let radius = 35.0;
     let theta = 1.0;
     let phi = 1.0;
-    let test_mat = mat4(
-        vec4(1.0, 3.0, 5.0, 0.0), 
-        vec4(1.0, 0.7, 0.9, 0.0),
-        vec4(2.0, 0.5, 0.2, 0.0),
-        vec4(0.0, 0.0, 0.0, 1.0)
-    );
 
     loop {
         clear_background(WHITE);
@@ -34,13 +28,15 @@ async fn main() {
             ..Default::default()
         });
 
+        // All temp. ASP this will all be a one call draw function.
         // Draw grid... duh
         draw_grid(20, 1.0, DARKGRAY, LIGHTGRAY);
 
-        // Draw a cylinder as the arm of the arrow.
+        // Major axis arrows
         general_shapes::draw_arrow_rot(vec3(0.0, -10.0, 0.0), vec3(0.0, 0.0, 0.0), 0.08, 0.25, 20.0, 0.95, None, LIGHTGRAY);
         general_shapes::draw_arrow_rot(vec3(-10.0, 0.0, 0.0), vec3(0.0, 0.0, -PI / 2.0), 0.08, 0.25, 20.0, 0.95, None, LIGHTGRAY);
         general_shapes::draw_arrow_rot(vec3(0.0, 0.0, 10.0), vec3(-PI / 2.0, 0.0, 0.0), 0.08, 0.25, 20.0, 0.95, None, LIGHTGRAY);
+
         set_default_camera();
         draw_text("I LOVE DANNYYYY", screen_height() / 2.0 - 60.0, 20.0, 30.0, DARKGRAY);
 
