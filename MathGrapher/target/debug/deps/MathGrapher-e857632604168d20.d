24e22ea53fc29aebbebff6eed158032ebfedaba5
Cristian Samuel Hernandez <craazycat213@gmail.com>
2026-08-13 13:04:08 -0400

    Up to date.

--- MathGrapher/target/debug/deps/MathGrapher-e857632604168d20.d
+++ MathGrapher/target/debug/deps/MathGrapher-e857632604168d20.d
@@ -1,6 +1,7 @@
-C:\Users\craaz\OneDrive\Documents\A-LegitimateCodeStuffs\MathGrapherEtc\MathGrapher\target\debug\deps\MathGrapher-e857632604168d20.d: src\main.rs src\general_shapes.rs
+C:\Users\craaz\OneDrive\Documents\A-LegitimateCodeStuffs\MathGrapherEtc\MathGrapher\target\debug\deps\MathGrapher-e857632604168d20.d: src\main.rs src\grid.rs src\general_shapes.rs
 
-C:\Users\craaz\OneDrive\Documents\A-LegitimateCodeStuffs\MathGrapherEtc\MathGrapher\target\debug\deps\libMathGrapher-e857632604168d20.rmeta: src\main.rs src\general_shapes.rs
+C:\Users\craaz\OneDrive\Documents\A-LegitimateCodeStuffs\MathGrapherEtc\MathGrapher\target\debug\deps\libMathGrapher-e857632604168d20.rmeta: src\main.rs src\grid.rs src\general_shapes.rs
 
 src\main.rs:
+src\grid.rs:
 src\general_shapes.rs:
