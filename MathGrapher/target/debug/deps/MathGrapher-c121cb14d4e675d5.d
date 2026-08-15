0601d946dd81389b597d045ba466029be6f7fbb9
Cristian Samuel Hernandez <craazycat213@gmail.com>
2026-08-15 15:13:12 -0400

    Up to date.

--- MathGrapher/target/debug/deps/MathGrapher-c121cb14d4e675d5.d
+++ MathGrapher/target/debug/deps/MathGrapher-c121cb14d4e675d5.d
@@ -1,7 +1,8 @@
-C:\Users\craaz\OneDrive\Documents\A-LegitimateCodeStuffs\MathGrapherEtc\MathGrapher\target\debug\deps\MathGrapher-c121cb14d4e675d5.d: src\main.rs src\grid.rs src\general_shapes.rs
+C:\Users\craaz\OneDrive\Documents\A-LegitimateCodeStuffs\MathGrapherEtc\MathGrapher\target\debug\deps\MathGrapher-c121cb14d4e675d5.d: src\main.rs src\grid.rs src\general_shapes.rs src\utils.rs
 
-C:\Users\craaz\OneDrive\Documents\A-LegitimateCodeStuffs\MathGrapherEtc\MathGrapher\target\debug\deps\libMathGrapher-c121cb14d4e675d5.rmeta: src\main.rs src\grid.rs src\general_shapes.rs
+C:\Users\craaz\OneDrive\Documents\A-LegitimateCodeStuffs\MathGrapherEtc\MathGrapher\target\debug\deps\libMathGrapher-c121cb14d4e675d5.rmeta: src\main.rs src\grid.rs src\general_shapes.rs src\utils.rs
 
 src\main.rs:
 src\grid.rs:
 src\general_shapes.rs:
+src\utils.rs:
--- MathGrapher/target/debug/deps/MathGrapher.d
+++ MathGrapher/target/debug/deps/MathGrapher.d
@@ -1,7 +1,8 @@
-C:\Users\craaz\OneDrive\Documents\A-LegitimateCodeStuffs\MathGrapherEtc\MathGrapher\target\debug\deps\MathGrapher.d: src\main.rs src\grid.rs src\general_shapes.rs
+C:\Users\craaz\OneDrive\Documents\A-LegitimateCodeStuffs\MathGrapherEtc\MathGrapher\target\debug\deps\MathGrapher.d: src\main.rs src\grid.rs src\general_shapes.rs src\utils.rs
 
-C:\Users\craaz\OneDrive\Documents\A-LegitimateCodeStuffs\MathGrapherEtc\MathGrapher\target\debug\deps\MathGrapher.exe: src\main.rs src\grid.rs src\general_shapes.rs
+C:\Users\craaz\OneDrive\Documents\A-LegitimateCodeStuffs\MathGrapherEtc\MathGrapher\target\debug\deps\MathGrapher.exe: src\main.rs src\grid.rs src\general_shapes.rs src\utils.rs
 
 src\main.rs:
 src\grid.rs:
 src\general_shapes.rs:
+src\utils.rs:
