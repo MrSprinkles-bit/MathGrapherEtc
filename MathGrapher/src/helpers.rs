2d8da89652de5e4ba6fec4a730ca4a2bad624e20
Cristian Samuel Hernandez <craazycat213@gmail.com>
2026-08-24 23:20:32 -0400

    Moved functions.

--- MathGrapher/src/helpers.rs
+++ MathGrapher/src/helpers.rs
@@ -1,5 +1,17 @@
 use macroquad::{math::EulerRot::XYZ, prelude::*};
 
+pub fn to_xzy(vec: Vec3) -> Vec3 {
+    return vec.xzy();
+}
+
+pub fn to_spherical(radius: f32, yaw: f32, pitch: f32) -> Vec3 {
+    return vec3(
+        radius * pitch.sin() * yaw.cos(),
+        radius * pitch.sin() * yaw.sin(),
+        radius * pitch.cos()
+    )
+}
+
 pub fn matrix_from_xyz(translation: Vec3, rotation: Vec3, scale: Vec3) -> Mat4 {
     let t_matrix = Mat4::from_translation(translation);
     let r_matrix = Mat4::from_euler(XYZ, rotation.x, rotation.y, rotation.z);
