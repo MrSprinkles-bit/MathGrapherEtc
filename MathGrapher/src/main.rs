806d8246bc0b79ad978f5ba07ab532ca88917567
Cristian Samuel Hernandez <craazycat213@gmail.com>
2026-08-24 23:20:52 -0400

    Major camera update.

--- MathGrapher/src/main.rs
+++ MathGrapher/src/main.rs
@@ -1,51 +1,27 @@
 mod grid;
 mod general_shapes;
 mod helpers;
-
-use std::f32::consts::PI;
+mod orbit_camera;
 
 use macroquad::prelude::*;
 
 use crate::grid::draw_graphing_area;
-
-fn camera_spherical_to_cartesian(radius: f32, mut pitch: f32, yaw: f32) -> Vec3 {
-    pitch = clamp(pitch, 0.0 + 0.0001, PI - 0.0001);
-    println!("{}", pitch);
-    return vec3(
-        radius * pitch.sin() * yaw.cos(),
-        radius * pitch.sin() * yaw.sin(),
-        radius * pitch.cos()
-    )
-}
-
-fn to_xzy(vec: Vec3) -> Vec3 {
-    return vec.xzy();
-}
+use crate::orbit_camera::OrbitCamera;
 
 #[macroquad::main("Testerrrrrr")]
 async fn main() {
-    let mut radius = 35.0;
-    let mut theta = 1.0;
-    let mut phi = 1.0;
+    let radius = 35.0;
+
+    let mut camera = OrbitCamera::new(
+        vec3(0.0,0.0,0.0), 
+        radius
+    );
 
     loop {
         clear_background(WHITE);
 
-        if mouse_wheel().1 != 0.0 {
-            radius += 0.5 * -mouse_wheel().1.signum();
-        }
-
-        if is_mouse_button_down(MouseButton::Left) {
-            phi -= mouse_delta_position().x;
-            theta += mouse_delta_position().y;
-        }
-
-        set_camera(&Camera3D {
-            position: to_xzy(camera_spherical_to_cartesian(radius, theta, phi)),
-            up: vec3(0.0, 1.0, 0.0),
-            target: vec3(0.0, 0.0, 0.0),
-            ..Default::default()
-        });
+        camera.update();
+        set_camera(&camera.to_macro_camera());
 
         draw_graphing_area();
 
---
+++ MathGrapher/src/orbit_camera.rs
@@ -0,0 +1,63 @@
+use std::f32::consts::PI;
+
+use macroquad::prelude::*;
+
+use crate::helpers;
+
+pub struct OrbitCamera {
+    target: Vec3,
+    distance: f32,
+    pitch: f32,
+    yaw: f32,
+    min_pitch: f32,
+    max_pitch: f32,
+    sensitivity: f32,
+    scroll_sens: f32,
+    fov: f32
+}
+
+impl OrbitCamera {
+    pub fn new(target: Vec3, distance: f32) -> Self {
+        // defaults
+        Self {
+            target,
+            distance,
+            pitch: 1.0,
+            yaw: 1.0,
+            min_pitch: 0.0 + 0.0001,
+            max_pitch: PI - 0.0001,
+            sensitivity: 1.0,
+            scroll_sens: 0.5,
+            fov: (45.0_f32).to_radians()
+        }
+    }
+
+    pub fn update(&mut self) {
+        if mouse_wheel().1 != 0.0 {
+            self.distance += self.scroll_sens * -mouse_wheel().1.signum();
+        }
+        
+        if is_mouse_button_down(MouseButton::Left) {
+            self.yaw -= mouse_delta_position().x * self.sensitivity;
+            self.pitch += mouse_delta_position().y * self.sensitivity;
+
+            self.pitch = self.pitch.clamp(self.min_pitch, self.max_pitch);
+        }
+    }
+
+    pub fn to_macro_camera(&self) -> Camera3D {
+        let pos = helpers::to_spherical(
+            self.distance, 
+            self.yaw, 
+            self.pitch
+        );
+
+        return Camera3D {
+            position: helpers::to_xzy(self.target + pos),
+            target: self.target,
+            up: vec3(0.0,1.0,0.0),
+            fovy: self.fov,
+            ..Default::default()
+        };
+    }
+}
