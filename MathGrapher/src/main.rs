e48b4f6acdac509acdea4baef2ac3b201119114b
Cristian Samuel Hernandez <craazycat213@gmail.com>
2026-08-25 22:20:38 -0400

    Tested function graphing ability.

--- MathGrapher/src/main.rs
+++ MathGrapher/src/main.rs
@@ -5,6 +5,7 @@ mod orbit_camera;
 
 use macroquad::prelude::*;
 
+use crate::general_shapes::draw_n_vertex_line;
 use crate::grid::draw_graphing_area;
 use crate::orbit_camera::OrbitCamera;
 
@@ -17,6 +18,12 @@ async fn main() {
         radius
     );
 
+    fn func(x: f32) -> f32 {
+        return x;
+    }
+
+    let res = 10;
+
     loop {
         clear_background(WHITE);
 
@@ -24,6 +31,20 @@ async fn main() {
         set_camera(&camera.to_macro_camera());
 
         draw_graphing_area(20);
+
+        let mut lines: Vec<Vertex> = Vec::new();
+
+        for x in (-20*res/2)..=(20*res/2) {
+            let xf = x as f32;
+            let resf = res as f32;
+            // y=f(x)
+            let z = -func(xf/resf);
+            //(x,f(x)) or i guess technically (x,0.,f(x))
+            lines.push(
+                Vertex::new2(vec3(xf/resf, 0.,z), vec2(0.,0.), RED)
+            );
+        }
+        draw_n_vertex_line(&lines);
 
         set_default_camera();
         draw_text("I LOVE DANNYYYYY", screen_height() / 2.0 - 60.0, 20.0, 30.0, DARKGRAY);
