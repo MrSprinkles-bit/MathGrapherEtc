49afcc11afd55b21f5018d64e80576f5e1afd2ba
Cristian Samuel Hernandez <craazycat213@gmail.com>
2026-08-26 23:10:33 -0400

    Up to date.

--- MathGrapher/target/debug/deps/MathGrapher-e857632604168d20.d
+++ MathGrapher/target/debug/deps/MathGrapher-e857632604168d20.d
@@ -1,9 +1,10 @@
-C:\Users\craaz\OneDrive\Documents\A-LegitimateCodeStuffs\MathGrapherEtc\MathGrapher\target\debug\deps\MathGrapher-e857632604168d20.d: src\main.rs src\grid.rs src\general_shapes.rs src\helpers.rs src\orbit_camera.rs
+C:\Users\craaz\OneDrive\Documents\A-LegitimateCodeStuffs\MathGrapherEtc\MathGrapher\target\debug\deps\MathGrapher-e857632604168d20.d: src\main.rs src\grid.rs src\general_shapes.rs src\helpers.rs src\orbit_camera.rs src\function_grapher.rs
 
-C:\Users\craaz\OneDrive\Documents\A-LegitimateCodeStuffs\MathGrapherEtc\MathGrapher\target\debug\deps\libMathGrapher-e857632604168d20.rmeta: src\main.rs src\grid.rs src\general_shapes.rs src\helpers.rs src\orbit_camera.rs
+C:\Users\craaz\OneDrive\Documents\A-LegitimateCodeStuffs\MathGrapherEtc\MathGrapher\target\debug\deps\libMathGrapher-e857632604168d20.rmeta: src\main.rs src\grid.rs src\general_shapes.rs src\helpers.rs src\orbit_camera.rs src\function_grapher.rs
 
 src\main.rs:
 src\grid.rs:
 src\general_shapes.rs:
 src\helpers.rs:
 src\orbit_camera.rs:
+src\function_grapher.rs:
--- MathGrapher/target/debug/deps/MathGrapher.d
+++ MathGrapher/target/debug/deps/MathGrapher.d
@@ -1,9 +1,10 @@
-C:\Users\craaz\OneDrive\Documents\A-LegitimateCodeStuffs\MathGrapherEtc\MathGrapher\target\debug\deps\MathGrapher.d: src\main.rs src\grid.rs src\general_shapes.rs src\helpers.rs src\orbit_camera.rs
+C:\Users\craaz\OneDrive\Documents\A-LegitimateCodeStuffs\MathGrapherEtc\MathGrapher\target\debug\deps\MathGrapher.d: src\main.rs src\grid.rs src\general_shapes.rs src\helpers.rs src\orbit_camera.rs src\function_grapher.rs
 
-C:\Users\craaz\OneDrive\Documents\A-LegitimateCodeStuffs\MathGrapherEtc\MathGrapher\target\debug\deps\MathGrapher.exe: src\main.rs src\grid.rs src\general_shapes.rs src\helpers.rs src\orbit_camera.rs
+C:\Users\craaz\OneDrive\Documents\A-LegitimateCodeStuffs\MathGrapherEtc\MathGrapher\target\debug\deps\MathGrapher.exe: src\main.rs src\grid.rs src\general_shapes.rs src\helpers.rs src\orbit_camera.rs src\function_grapher.rs
 
 src\main.rs:
 src\grid.rs:
 src\general_shapes.rs:
 src\helpers.rs:
 src\orbit_camera.rs:
+src\function_grapher.rs:
